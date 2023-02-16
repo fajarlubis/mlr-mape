bf7b6d06b1ccaa7c3fba163b7cfc7edf38116600
Fajar Lubis <fajar@ddtc.co.id>
2023-02-16 12:00:48 +0700

    add comments

--- main.go
+++ main.go
@@ -14,12 +14,14 @@ import (
 	"github.com/xuri/excelize/v2"
 )
 
+// struct untuk menampung data file csv
 type Result struct {
 	Items []*ResultItem
 	Type  string
 	MAPE  float64
 }
 
+// struct untuk menampung data item-item per bulan file csv
 type ResultItem struct {
 	Year       int
 	Month      int
@@ -27,49 +29,55 @@ type ResultItem struct {
 }
 
 func main() {
-	// Load the dataset from a CSV file
+	// memuat dataset dari file csv
 	file, err := os.Open("training-data.csv")
 	if err != nil {
 		log.Fatal(err)
 	}
+	// fungsi defer file.Close() untuk menunda close buffer dari os.Open setelah selesai mengeksekusi function keseluruhan
 	defer file.Close()
 
 	reader := csv.NewReader(file)
-	records, err := reader.ReadAll()
+	records, err := reader.ReadAll() // proses membaca isi file csv
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	var (
+		// definisi variable baru dari struct diatas
 		res   = make([]*Result, 0)
 		items = make([]*ResultItem, 0)
 	)
 
-	for label := 0; label <= 10; label++ { // there is 11 data label
-		// Extract the input and output data from the dataset
+	for label := 0; label <= 10; label++ { // ada 11 data label mulai dari kWh penerimaan sampai susut non-teknis, di buat 10 karena index dimulai dari 0
+
+		// ekstrak input data dari file csv
 		var (
+			// definisi variable x, y dan kWhType
 			x       [][]float64
 			y       []float64
 			kWhType string
 		)
 
-		for _, record := range records {
-			var input []float64
-			for i := 0; i < len(record)-11; i++ {
-				val, err := strconv.ParseFloat(record[i], 64)
+		for _, record := range records { // loop setiap data yang sudah di tampung di variable yang sudah di definisikan sebelumnya
+			var input []float64                   // definisi variable baru untuk nilai input float64
+			for i := 0; i < len(record)-11; i++ { // loop 2 kolom pertama yang berisikan bulan dan tahun
+				val, err := strconv.ParseFloat(record[i], 64) // konversi isi data ke bentuk float64
 				if err != nil {
-					log.Fatal(err)
+					log.Fatal(err) // jika bulan dan tahun bukan dalam bentuk angka maka return error
 				}
-				input = append(input, val)
+				input = append(input, val) // jika bulan dan tahun dalam bentuk angka maka sematkan ke dalam variable input
 			}
-			x = append(x, input)
+			x = append(x, input) // setelah loop 1 row selesai maka variable x diisi dengan hasil dari loop bulan dan tahun
 
-			yOut, err := strconv.ParseFloat(record[label+2], 64)
+			yOut, err := strconv.ParseFloat(record[label+2], 64) // yOut adalah isi dari setiap cell yang ada di dalam file csv, dimulai dari label+2 yang berarti jika index sekarang 0 maka dimulai dari kolom 2 (2+0)
 			if err != nil {
-				log.Fatal(err)
+				log.Fatal(err) // jika konversi nilai gagal maka return error
 			}
-			y = append(y, yOut)
+			y = append(y, yOut) // setelah loop 1 row selesai maka variable y diisi dengan nilai dari setiap cell yang ada di dalam file csv
 
+			// switch label + 2 menentukan jenis/label dari setiap nilai, label + 2 menandakan posisi kolom saat ini, +2 karena 2 kolom pertama bukan berisi nilai melainkan bulan dan tahun
+			// jika nilai sesuai dengan masing-masing case maka variable kWhType akan berisi dengan nama dari masing-masing label di setiap nilainya
 			switch label + 2 {
 			case 2:
 				kWhType = "kWh Penerimaan"
@@ -96,60 +104,59 @@ func main() {
 			}
 		}
 
-		// Train the linear regression model
-		r := new(regression.Regression)
-		r.SetObserved("Energy loss every year")
-		r.SetVar(0, "month")
-		r.SetVar(1, "year")
+		// training linear regression model
+		r := new(regression.Regression) // inisialisasi regressi baru
+		r.SetObserved("Nilai kWh")      // set label jenis variable yang akan di observasi/diproses
+		r.SetVar(0, "month")            // set variable month(bulan) dengan index pertama 0
+		r.SetVar(1, "year")             // set variable year(tahun) dengan index kedua 1
 
-		for i, xi := range x {
-			r.Train(
-				regression.DataPoint(y[i], xi),
+		for i, xi := range x { // loop berdasarkan sumbu x dengan data-data didalamnya
+			r.Train( // mempersiapkan training data sesuai nilai x dan y yang sudah diberikan
+				regression.DataPoint(y[i], xi), // menambahkan datapoint ke dalam proses training
 			)
 		}
 
-		r.Run()
+		r.Run() // start training data sesuai dengan instruksi dan nilai yang telah diberikan
 
 		// fmt.Printf("Regression formula:\n%v\n", r.Formula)
 		// fmt.Printf("Regression:\n%s\n", r)
 
-		fmt.Printf("R^2:\n%v\n", r.R2)
+		fmt.Printf("R^2:\n%v\n", r.R2) // print hasil R^2 dari hasil training ke layar
 
-		// Use the trained model to make predictions for the next 5 years
+		// inisialisasi variable untuk menampung data prediksi untuk 5 tahun kedepan
 		var predictions []float64
-		for year := 2021; year <= 2025; year++ {
-			for month := 1; month <= 12; month++ {
-				input := []float64{float64(month), float64(year)}
-				prediction, err := r.Predict(input)
+		for year := 2021; year <= 2025; year++ { // loop jarak tahun yang akan di prediksi 2021 - 2025
+			for month := 1; month <= 12; month++ { // loop setiap bulan 1 - 12
+				input := []float64{float64(month), float64(year)} // mengambil data bulan dan tahun dan mengisi kedalam variable slice baru
+				prediction, err := r.Predict(input)               // prediksi sesuai data yang sudah di training
 				if err != nil {
-					log.Fatal(err)
+					log.Fatal(err) // jika error maka return dan keluarkan error di layar
 				}
-				predictions = append(predictions, prediction)
+				predictions = append(predictions, prediction) // menyematkan hasil prediksi ke dalam variable predictions
 			}
 		}
 
-		// Calculate the mean absolute percentage error of the predictions
+		// inisialisasi menghitung Mean Absolute Percentage Error (MAPE)
 		var actual []float64
 		var absoluteErrors []float64
-		actual = append(actual, y...)
+		actual = append(actual, y...) // mengambil data aktual hasil training dan menyematkan ke dalam variable baru actual
 
-		for i, value := range predictions {
-			absoluteError := 100 * (actual[i] - value) / actual[i]
-			absoluteErrors = append(absoluteErrors, absoluteError)
+		for i, value := range predictions { // loop data prediksi
+			absoluteError := 100 * (actual[i] - value) / actual[i] // mulai menghitung MAPE sesuai rumus
+			absoluteErrors = append(absoluteErrors, absoluteError) // menyematkan data hasil MAPE ke dalam variable absoluteErrors
 		}
 
-		sum := 0.0
-		for _, error := range absoluteErrors {
-			sum += error
+		sum := 0.0                            // nilai sum awal 0.0 default
+		for _, errx := range absoluteErrors { // loop absoluteErrors dan menambahkan total absolute error
+			sum += errx
 		}
 		meanAbsolutePercentageError := sum / float64(len(absoluteErrors))
 
-		// Print the predictions and the mean absolute percentage error
-		// fmt.Println("Predictions:")
+		// menyematkan data hasil prediksi dan MAPE ke dalam variable predictions untuk dapat di proses ke tahap selanjutnya
 		for i, prediction := range predictions {
 			items = append(items, &ResultItem{
-				Year:       (i / 12) + 2021,
-				Month:      (i % 12) + 1,
+				Year:       (i / 12) + 2021, // tahun mulai yang akan di prediksi
+				Month:      (i % 12) + 1,    // bulan dalam angka
 				Prediction: prediction,
 			})
 
@@ -158,6 +165,7 @@ func main() {
 
 		// fmt.Printf("Mean absolute percentage error: %.2f%%\n", meanAbsolutePercentageError)
 
+		// menyematkan data setiap item ke dalam variable result
 		res = append(res, &Result{
 			Items: items,
 			Type:  kWhType,
@@ -165,15 +173,9 @@ func main() {
 		})
 	}
 
-	// for _, v := range res {
-	// 	for _, w := range v.Items {
-	// 		fmt.Printf("%d-%02d: %.2f\n", w.Year, w.Month, w.Prediction)
-	// 	}
-	// 	fmt.Printf("Mean absolute percentage error: %.2f%%\n", v.MAPE)
-	// }
-
+	// ekspor hasil training ke dalam bentuk file xlsx
 	if err := export2(res); err != nil {
-		log.Fatalln(err)
+		log.Fatalln(err) // jika error maka data tidak akan muncul di file result.xlsx
 	}
 }
 
